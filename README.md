2cf254527bfa2ff6f3ab31cf601c7fe7c4a7352b
nathenharvey <nharvey@opscode.com>
2014-08-07 01:42:55 -0400

    a working solution

---
+++ README.md
@@ -0,0 +1,9 @@
+# mongodb
+
+* `chef generate cookbook mongodb`
+* `chef generate template mongodb.repo`
+
+* write the `default.rb` recipe
+* write the `mongodb.repo.erb` template
+
+* `chef-client -z -r mongodb`
---
+++ metadata.rb
@@ -0,0 +1,8 @@
+name             'mongodb'
+maintainer       ''
+maintainer_email ''
+license          ''
+description      'Installs/Configures mongodb'
+long_description 'Installs/Configures mongodb'
+version          '0.1.0'
+
