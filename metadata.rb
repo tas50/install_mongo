9ee6d763427fb8f370d2be367c4d8c1549c11050
Tim Smith <tsmith84@gmail.com>
2015-10-20 21:04:28 -0700

    Rubocop fixes and other minor updates

--- metadata.rb
+++ metadata.rb
@@ -5,4 +5,3 @@ license          ''
 description      'Installs/Configures mongodb'
 long_description 'Installs/Configures mongodb'
 version          '0.1.0'
-
--- recipes/default.rb
+++ recipes/default.rb
@@ -4,25 +4,25 @@
 #
 
 # Create a /etc/yum.repos.d/mongodb.repo file to hold the configuration
-baseurl = "http://downloads-distro.mongodb.org/repo/redhat/os/x86_64/"
+baseurl = 'http://downloads-distro.mongodb.org/repo/redhat/os/x86_64/'
 
-if node["kernel"]["machine"] != "x86_64"
-  baseurl = "http://downloads-distro.mongodb.org/repo/redhat/os/i686/"
+if node['kernel']['machine'] != 'x86_64'
+  baseurl = 'http://downloads-distro.mongodb.org/repo/redhat/os/i686/'
 end
 
-template "/etc/yum.repos.d/mongodb.repo"  do
+template '/etc/yum.repos.d/mongodb.repo' do
   variables(
-    :baseurl => baseurl
+    baseurl: baseurl
   )
 end
 
 # Install the MongoDB packages and associated tools.
-package "mongodb-org" do
+package 'mongodb-org' do
   action :install
 end
 
 # Start MongoDB.
 # ensure that MongoDB will start following a system reboot
-service "mongod" do
+service 'mongod' do
   action [:start, :enable]
 end
